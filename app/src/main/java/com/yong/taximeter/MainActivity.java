55a029dcb2131e08bcd7fb96ee65393b84ff6563
yymin1022 <yymin1022@gmail.com>
2020-09-09 21:38:34 +0900

    Update MainActivity : Add Theme Setting Dialog

--- app/src/main/java/com/yong/taximeter/MainActivity.java
+++ app/src/main/java/com/yong/taximeter/MainActivity.java
@@ -1,11 +1,14 @@
 package com.yong.taximeter;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +42,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void theme(View v){
-        startActivity(new Intent(this, SettingsActivity.class));
+        final String[] themeList = {"Horse", "Circle"};
+        final int[] selectedItem = {0};
+
+        AlertDialog.Builder themeDialog = new AlertDialog.Builder(MainActivity.this);
+        themeDialog.setTitle("Set Theme");
+        themeDialog.setSingleChoiceItems(themeList, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int index) {
+                selectedItem[0] = index;
+            }
+        });
+        themeDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), String.valueOf(selectedItem[0]), Toast.LENGTH_LONG).show();
+                dialogInterface.dismiss();
+            }
+        });
+        themeDialog.create().show();
     }
 }
